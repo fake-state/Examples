b8ac12c1fb0d54b241646e15157878af59fbd509
fake_state <ntdako@gmail.com>
2023-02-09 00:23:39 +0400

    example 006 base

--- example_006_base/Program.cs
+++ example_006_base/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿// Нахождение максимального числа из представленных
+
+int a = 1;
+int b = 4;
+int c = 8;
+int d = 5;
+int e = 3;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("Max = ");
+Console.WriteLine(max);

e2f608add411c3388d198ef40dc00e78ac96d849
fake_state <ntdako@gmail.com>
2023-02-09 00:08:59 +0400

    example 005 if else

---
+++ Example_005_If_Else/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "алена")
+{
+  Console.WriteLine("Ура, это же Алена!");
+}
+else
+{
+  Console.Write("Привет," + " " + username);
+}

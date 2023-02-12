3270b1dfd84aa289d12b656c62b1f58068b1ea1b
fake_state <ntdako@gmail.com>
2023-02-12 17:04:07 +0400

    add //

---
+++ practic/Program.cs
@@ -0,0 +1,25 @@
+﻿//Console.Clear();
+//int n = new Random().Next(10, 100); // [10, 99]
+//int n1 = n / 10; // 98 / 10 = 9
+//int n2 = n % 10; // 98 % 10 = 8
+//Console.WriteLine($"Число: {n}");
+
+//if (n1 > n2)
+//    Console.WriteLine(n1);
+//else if (n2 > n1)
+//    Console.WriteLine(n2);
+//else
+//    Console.WriteLine("=");
+
+//int n = Convert.ToInt32(Console.ReadLine ());
+//if ((n % 7 == 0) && (n % 23 == 0)) Console.WriteLine("Yes");
+//else
+//Console.WriteLine("Neeee");
+
+
+//int a = Convert.ToInt32(Console.ReadLine ());
+//int b = Convert.ToInt32(Console.ReadLine ());
+
+//if (b == a*a || a == b*b) Console.WriteLine("Да");
+//else
+//Console.WriteLine("Нет");

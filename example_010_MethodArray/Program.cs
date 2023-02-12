7d14164dab3d66932a8ae2706d0f9b5337f3bec8
fake_state <ntdako@gmail.com>
2023-02-12 14:26:06 +0400

    add array and code

---
+++ example_010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 1, 21, 3, 41, 15, 6, 17, 81, 19};
+int n = array.Length;
+int find = 81;
+int index = 0;
+
+while (index < n)
+{
+  if (array[index] == find)
+  {
+    Console.WriteLine(index);
+  }
+index++;
+}
